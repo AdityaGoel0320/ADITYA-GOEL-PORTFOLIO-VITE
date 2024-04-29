b2cc05abb567bd9e5ccc902da1a89c692f61a3ad
Aditya Goel <adityagoelgithub0320@gmail.com>
2024-04-29 19:58:57 +0530

    major updates in databse and frontend

--- src/assets/FrontEndUtils.jsx
+++ src/assets/FrontEndUtils.jsx
@@ -13,10 +13,10 @@ export const desc = (
 
                 <h1>Certicate <i class="fa-solid fa-arrow-up-right-from-square"></i></h1>
             </a>
-            <a href="">
+            {/* <a href="">
 
                 <h1>Project Report <i class="fa-solid fa-arrow-up-right-from-square"></i></h1>
-            </a>
+            </a> */}
         </div>
         Implemented and learned <strong>MERN stack authentication</strong> with <strong>JWT</strong>,
         led <strong>Swagger Docs</strong> for <strong>API clarity</strong>,
@@ -33,12 +33,12 @@ export const desc = (
 export const HacktoberFestDesc = (
     <div>
         <div className="linksCertificate">
-            <a href="https://drive.google.com/file/d/13csFfVIFXCmLTlQ-kjAdgbX2yUEs6vd3/view?usp=sharing" target="_blank">
+            {/* <a href="https://drive.google.com/file/d/13csFfVIFXCmLTlQ-kjAdgbX2yUEs6vd3/view?usp=sharing" target="_blank">
                 <h1>Certificate <i class="fa-solid fa-arrow-up-right-from-square"></i></h1>
-            </a>
-            <a href="">
+            </a> */}
+            {/* <a href="">
                 <h1>Project Report <i class="fa-solid fa-arrow-up-right-from-square"></i></h1>
-            </a>
+            </a> */}
         </div>
         Completed <strong>Hacktoberfest 2022</strong>, which served as my gateway into the world of <strong>open source</strong>. This journey expanded my knowledge about contributing in public, adopting <strong>good coding practices</strong>, and delving deep into <strong>GitHub workflows</strong>. Through Hacktoberfest, I established <strong>strong connections</strong> within the <strong>open-source community</strong>, gaining valuable insights into <strong>collaborative development</strong> and the importance of <strong>effective communication</strong>. Furthermore, I honed my skills in <strong>version control</strong>, <strong>pull requests</strong>, and <strong>issue tracking</strong>, solidifying my understanding of the <strong>open-source ecosystem</strong>. This experience was not just about coding but also about fostering a culture of <strong>inclusivity</strong>, <strong>sharing</strong>, and <strong>continuous learning</strong> within the <strong>developer community</strong>.
     </div>
---
+++ src/components/ProjectArr.js
@@ -0,0 +1,60 @@
+const ProjectArr = [
+
+    {
+        name: "ExpeditionXplorer.com",
+        img: "./images/ExpeditionXplorer.png",
+        repo: "https://github.com/AdityaGoel0320/ExpeditionXplorer-Reactjs",
+        link: "https://expedition-xplorer-reactjs.vercel.app/",
+        desc: "This website showcases my ReactJS skills in managing API-based data and state management. It also demonstrates my ability to design a real-life travel website using CSS. The result is a beautifully crafted product that I'm proud of. ",
+        techUsed: ["ReactJS", "JavaScript", "HTML", "CSS" , "TailWindCss"],
+    },
+    {
+        name: "Reactjs Advanced Cart System",
+        img: "./images/cart.png",
+        repo: "https://github.com/AdityaGoel0320/Reactjs-Advanced-Cart-System",
+        link: "https://reactjs-advanced-cart.vercel.app/",
+        desc: "This is a highly realistic cart system. In this project, I have utilized advanced state management techniques to create a scalable cart and make all data dynamic. All products are fetched using API data. I have also employed various hooks for advanced state management, such as useEffect(), useContext(), useState(), and many other JavaScript concepts.",
+        techUsed: ["ReactJS", "JavaScript", "HTML", "CSS"],
+    }
+    , 
+    {
+        name: "Dynamic Pizza Paradise: A React-Tailwind Delight !",
+        img: "./images/pizza.png",
+        repo: "https://github.com/AdityaGoel0320/Pizza-Store-ReactJs",
+        link: "https://pizza-store-react-js.vercel.app/",
+        desc: "Developed a dynamic and interactive Pizza Shop web using React.js,Tailwind CSS, HTML, and CSS. Utilized React hooks(useState, useEffect)and JavaScript functions(filter(), map()) forefficient data handling and enhanced userexperience.",
+        techUsed: ["ReactJS", "JavaScript", "HTML", "CSS" , "TailWindCss"],
+    },
+
+
+    {
+        name: "Task Manager Pro: Task Mastery with React Magic!",
+        img: "./images/todo.png",
+        repo: "https://github.com/AdityaGoel0320/ReactJs-Todo-App",
+        link: "https://react-js-todo-app-one.vercel.app/",
+        desc: "Created a Task Manager application with React.js, Tailwind CSS, HTML, and CSS, featuring local storage for data storage Utilized React hooks for managing state and building a responsive user interface Nostalgia Arcade: Relive Classics in Modern Style!",
+        techUsed: ["ReactJS", "JavaScript", "HTML", "CSS"],
+    },
+
+    {
+        name: "Nostalgia Arcade: Relive Classics in Modern Style!",
+        img: "./images/game.png",
+        repo: "https://github.com/AdityaGoel0320/Game_center_Javascript",
+        link: "https://game-center-javascript.vercel.app/",
+        desc: "Transform your passion for gaming into a mesmerising web experience with our Game Center app, where classic games meet modern JavaScript mastery. Engage users with dynamic gameplay, responsive design, and a touch of nostalgia.🎮🌟",
+        techUsed: [ "JavaScript", "HTML", "CSS", ],
+    },
+  
+    // {
+    //     name: "",
+    //     img: "./images/",
+    //     repo: "",
+    //     link: "",
+    //     desc: "",
+    //     techUsed: ["ReactJS", "JavaScript", "HTML", "CSS"],
+    // },
+
+   
+  ];
+
+export default ProjectArr;
